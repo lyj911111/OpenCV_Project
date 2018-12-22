e70adf139160ac9946f87a46454c82678b949461
Jay Lee <lyj911111@naver.com>
2018-12-22 15:48:51 +0700

    메인 통합 테스트 완료

--- AceVision/main.py
+++ AceVision/main.py
@@ -1,18 +1,26 @@
 import sys
 from RivetDetect import RivetDetect
-#from QRDetect import QRDetect
+from QRDetect import QRDetect
 
-isExe = sys.argv[0]
+#isExe = sys.argv[0]
+
+# 테스트중...
+# 이곳에 입력한 모듈 (.py)을 실행함.
+test = 2
 
 def main():
 
     # if isExe == 1:
     # elif isExe == 2:
-
-    RivetDetect.execute()
+    
+    if test == 1:
+        RivetDetect.execute()
+    elif test == 2:
+        QRDetect.execute()
 
     return
 
     
 if __name__ == "__main__":
     main()
+    
