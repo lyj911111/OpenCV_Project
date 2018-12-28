725a38551c59bfa3cda96a8e8bee78706caf1d29
Jay Lee <lyj911111@naver.com>
2018-12-28 08:16:16 +0700

    원모양 동영상 탐지

--- _testcodes/원모양 탐지/원모양탐지 동영상.py
+++ _testcodes/원모양 탐지/원모양탐지 동영상.py
@@ -6,32 +6,36 @@ from PIL import Image
 import numpy as np
 
 
-def houghCircle():
-    img1 = cv2.imread('circle2.jpg')    # 원형이 있는 이미지를 올림.
-    img2 = img1.copy()
+cap = cv2.VideoCapture(1)
 
-    img2 = cv2.GaussianBlur(img2, (9, 9), 0) # 0~9까지 가우시안 필터로 흐리게 만들어 조절함.
+while True:
+    _, frame = cap.read()
 
-    imgray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY) # 그레이 이미지로 바꿔서 실행해야함.
+    copy_frame = frame.copy()                               # 다듬기 위해 원본 복사
+    copy_frame = cv2.GaussianBlur(copy_frame, (3, 3), 0)    # 복사본에 가우시안 필터
+    gray = cv2.cvtColor(copy_frame, cv2.COLOR_BGR2GRAY)     # 필터위에 그레이로 씌워줌
 
-                                 # 원본과 비율 / 찾은 원들간의 최소 중심거리 / param1, param2를 조절해 원을 찾음
-    circles = cv2.HoughCircles(imgray, cv2.HOUGH_GRADIENT, 1, 10, param1=60, param2=50, minRadius=0, maxRadius=0)
+    #_, thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY_INV)
 
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 60, param1=60, param2=50, minRadius=0, maxRadius=0) # 그레이 이미지에서 원형 검출
+
+    # 원형 탐지
     if circles is not None:
         circles = np.uint16(np.around(circles))
 
-        print(circles)
+        print(circles) # 탐지된 원의 리스트 출력
 
+        # 탐지된 원의 외곽선을 그려줌
         for i in circles[0, :]:
-            cv2.circle(img1, (i[0], i[1]), i[2], (255, 255, 0), 2)
+            cv2.circle(frame, (i[0], i[1]), i[2], (255, 255, 0), 2)
 
-        cv2.imshow('ori',img2)
-        cv2.imshow('HoughCircle', img1)
-
-
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
     else:
         print('원을 찾을 수 없음')
 
-houghCircle()
+    cv2.imshow('gray', gray)
+    cv2.imshow('fr', frame)         # 그레이에서 추출한 원을 표시
+
+
+    # 종료키
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
