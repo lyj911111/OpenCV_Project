4f54ab4912ad613bd650761726c144018b761c16
Jay Lee <lyj911111@naver.com>
2019-01-09 14:51:38 +0700

    1층 1/9 테스트, 기구에 달아서 테스트

--- AceVision_GUI_Version/QR_SS_Detect/QR_SS_Detect.py
+++ AceVision_GUI_Version/QR_SS_Detect/QR_SS_Detect.py
@@ -23,7 +23,7 @@ cap0.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 cap1 = cv2.VideoCapture(2)
 cap1.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-cap2 = cv2.VideoCapture(3)
+cap2 = cv2.VideoCapture(0)
 cap2.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
@@ -60,6 +60,18 @@ ss_value_list = ['', " ", " ", " ", " ", " "]
 #ss_value_list[0] = NONE
 Serial_No = ''
 
+PLC_signal = False
+check_set = True
+box_set = False
+count_mouse = 0
+
+qr_x1 = 0
+qr_y1 = 0
+qr_x2 = 0
+qr_y2 = 0
+
+pathname = ''
+
 
 #def decode(image) :
 #    pass
@@ -85,8 +97,9 @@ def webCamShow_SS(N, Display):
     Display.configure(image=imgtk)
 
 def read_frame():
-    global qr_label, sticker_label, Serial_No, check_serial_no
+    global qr_label, sticker_label, Serial_No, check_serial_no, box_label
     global qr_check_result, ss_check_result, qr_check_process, ss_check_process
+    global box_set
 
     print("======== read =========")
     print("Serial_No : ", Serial_No)
@@ -112,7 +125,23 @@ def read_frame():
 
     webCamShow_QR(cap1.read(), qr_label)
     webCamShow_SS(cap0.read(), sticker_label)
+
+
+    if box_set == True:
+        image = qr_image()
+        image_reformat = Reformat_Image(image)
+        imageShow(image_reformat, box_label)
+        box_set = False
+
     root.after(10, read_frame)
+
+
+def qr_image():
+    global frame_cp
+    global qr_x1, qr_x2, qr_y1, qr_y2
+
+    return frame_cp[(qr_y1):(qr_y2), (qr_x1):(qr_x2)]
+
 
 def get_today():
     global Serial_No
@@ -184,12 +213,24 @@ def leave_log(check_function):
     f.write(data)
     f.close()
 
+def imageShow(N, Display):
+    frame = N
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    cv2image = Img.fromarray(cv2image)
+    imgtk =ImageTk.PhotoImage(image=cv2image)
+    Display.imgtk = imgtk
+    Display.configure(image=imgtk)
+
+
 
 
 def Reformat_Image(image):
     height, width = image.shape[:2]
+    print(width, height)
     width = int(width*1.2)
     height = int(height*1.2)
+    if height == 0:
+        height = width
     res = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
 
     return res
@@ -200,11 +241,16 @@ def QRDetect(frame):
     global Serial_No, QR_SN
     global qr_cx, qr_cy, qr_check_result
     global qr_check_process, ss_check_process
+    global qr_x1, qr_y1, qr_x2, qr_y2
+    global frame_cp, PLC_signal
+    global point_x1, point_x2, point_y1, point_y2
 
+    '''
     qr_x1 = 141
     qr_y1 = 236
     qr_x2 = 254
     qr_y2 = 348
+    '''
 
     frame = cv2.flip(frame, 0)
     frame = cv2.flip(frame, 1)
@@ -213,10 +259,11 @@ def QRDetect(frame):
     frame3 = frame.copy()
 
     frame4 = frame.copy()
-
+    frame_cp = frame.copy()
     cv2.rectangle(frame3, (qr_x1, qr_y1), (qr_x2, qr_y2), (255, 0, 0), 3)
     cx_ref = int(abs(qr_x1 + qr_x2) / 2)
     cy_ref = int(abs(qr_y1 + qr_y2) / 2)
+    qr_area = abs(qr_x2 - qr_x1) * abs(qr_y2 - qr_y1)
     cv2.circle(frame3, (cx_ref, cy_ref), 3, (255, 0, 0), -1)
 
     frame = cv2.GaussianBlur(frame, (3, 3), 0)
@@ -309,73 +356,54 @@ def QRDetect(frame):
             qr_cx = int(abs(box_x[0] + box_x[2]) / 2)
             qr_cy = int(abs(box_y[0] + box_y[2]) / 2)
             Area = dx * dy
+            print("Area", Area)
+            box_x.sort()
+            box_y.sort()
+            point_x1 = box_x[0]
+            point_x2 = box_x[3]
+            point_y1 = box_y[0]
+            point_y2 = box_y[3]
         elif (len(box_x) == 2) and (len(box_y) == 2):
             dx = int(abs(box_x[0] - box_x[1]))
             dy = int(abs(box_y[0] - box_y[1]))
             qr_cx = int(abs(box_x[0] + box_x[1]) / 2)
             qr_cy = int(abs(box_y[0] + box_y[1]) / 2)
             Area = dx * dy
+            print("Area", Area)
+            box_x.sort()
+            box_y.sort()
+            point_x1 = box_x[0]
+            point_x2 = box_x[1]
+            point_y1 = box_y[0]
+            point_y2 = box_y[1]
 
         cv2.circle(frame3, (qr_cx, qr_cy), 3, (0, 255, 0), -1)
-        # print(cx, cy)
-        # print(Area)
-        # print(box)
 
         cv2.putText(frame2, "%d" % qr_cx, (400, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 1)
-        # cv2.putText(frame2, "%d" % cy, (400, 200), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 1)
-        # cv2.putText(frame2, "%d" % Area, (400, 300), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 1)
 
         box_x0 = box_x[0] < box_x[1] and box_x[0] or box_x[1]
-        box_y1 = box_y[0] > box_y[1] and box_y[0] or box_y[1]
+        #box_y1 = box_y[0] > box_y[1] and box_y[0] or box_y[1]
         box_y0 = box_y[0] < box_y[1] and box_y[0] or box_y[1]
 
         dx = qr_cx - box_x0
         dy = qr_cy - box_y0
 
         degree = int(atan2(dx, dy) * 180 / pi)
-        # print(degree)
-        # cv2.putText(frame3, "%d" % degree, (300, 300), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 3)
 
-        # 시리얼 번호 입력창에 입력된 데이터 저장
-        # Serial_No = SN.get()
-        #Serial_No = QR_SN.get()
-        #print("QR 시리얼 넘버: ", Serial_No)
         fn = datetime.datetime.now()
         folder_name = str(fn.year) + "-" + str("%02d" % fn.month) + "-" + str("%02d" % fn.day)
-        # print(box_x0, qr_x1)
-        print("========= QR ===========")
-        print("process :", qr_check_process, "result : ", qr_check_result)
-        print("Serial_No : ",Serial_No)
-        print("len : ", len(SS_SN.get()), len(QR_SN.get()))
-        if (len(SS_SN.get()) != 0 or len(QR_SN.get()) != 0):
-            print("테스트")
-        if abs(qr_cx - cx_ref) <= 1 and (10000 <= Area and Area <= 18000)  and qr_check_process == 0 and qr_check_result == 2:
-            # if abs(int(box_x0) - int(qr_x1)) <= 2 and 10000<=Area and Area <= 18000:
 
-            cv2.imwrite("./barcode_read.jpg", frame3)
+        cv2.putText(frame3, "%d" % degree, (400, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 1)
+        if abs(qr_cx - cx_ref) <= 3 and qr_check_process == 0 and qr_check_result == 2 and PLC_signal == True:
 
-            print(qr_cx, qr_cy)
-            #roi = frame4[(qr_cx-56)-100+191-20:(qr_cx+56)-100+191+20, (qr_cy-66)-100-20:(qr_cy+66)-100+10]
-            #roi = frame4[(qr_cx +15):(qr_cx + 167),(qr_cy - 186):(qr_cy -24)]
-            roi = frame4[(qr_cx + 0):(qr_cx + 190), (qr_cy - 190):(qr_cy - 15)]
-            #roi = cv2.copyMakeBorder(roi,3,3,3,3,cv2.BORDER_CONSTANT,value=[255,255,255])
+            roi = frame4[(point_y1  - 30):(point_y2 + 30), (point_x1 - 30):(point_x2 + 30)]
             qr_image = Reformat_Image(roi)
 
-            #print("이미지 저장")
-            #test = cv2.imread("./barcode_read.jpg")
-            #cv2.imshow("test", test)
-
             cv2.imshow("qr_image", qr_image)
-
-            #cv2.imwrite("./test.jpg", test)
             data = str(decode(qr_image))
-            print(data)
-            #print(data)
             sn = data[24:37]
-            print(sn)
             Serial_No = sn
             QR_SN.insert(20, Serial_No)
-
 
             print("=====  판독 중  =====")
             qr_check_process = 1
@@ -393,8 +421,15 @@ def QRDetect(frame):
 
             # print(abs(cx- cx_ref), abs(box_y[0]-qr_y2), abs(box_y[1] - qr_y1))
             # if abs(cx- cx_ref) <= 5 and abs(box_y0-qr_y1) <= 7 and abs(box_y1 - qr_y2) <= 7 :
+            #print("box_x0, qr_x1, box_y0, qr_y1, box_y1, qr_y2", box_x0, qr_x1, box_y0, qr_y1, box_y1, qr_y2)
+            #print(abs(int(box_x0) - int(qr_x1)))
+            ##print(abs(box_y0 - qr_y1))
+            #print(abs(box_y1 - qr_y2))
+
+            cv2.imwrite("image.jpg", frame3)
+
             if (40 < degree and degree < 50) and abs(int(box_x0) - int(qr_x1)) <= 7 \
-                    and abs(box_y0 - qr_y1) <= 7 and abs(box_y1 - qr_y2) <= 7:
+                    and abs(box_y0 - qr_y1) <= 7:
                 cv2.putText(frame3, "PASS", (qr_cx - 100, qr_cy - 50), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 5)
                 cv2.imwrite(store_location + "%s/barcode/pass/%s.jpg" % (folder_name, Serial_No), frame3)
                 # cv2.imwrite(store_location + "/%s/pass/qr%d" % (folder_name, count_pass), frame3)
@@ -462,7 +497,7 @@ def SSDetect(frame):
     global Serial_No
     global ss_cx, ss_cy, ss_check_result
     global qr_check_process, ss_check_process
-    global cnts
+    global cnts, PLC_signal
 
     def filter(frame):
         global cnts
@@ -607,7 +642,7 @@ def SSDetect(frame):
         for pt in zip(*loc[::-1]):
             cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 1)
 
-            sensitivity = 700
+            sensitivity = 650
             # sensitivity = 580 => 확인 후 값 고정
             f.add((round(pt[0] / sensitivity), round(pt[1] / sensitivity)))
         found_count = len(f)
@@ -621,20 +656,11 @@ def SSDetect(frame):
         fn = datetime.datetime.now()
         folder_name = str(fn.year) + "-" + str("%02d" % fn.month) + "-" + str("%02d" % fn.day)
 
-        # print(Area)
-        # if (60000 <= area and area <= 75000):
-        # if abs(cx - cx_ref) <= 1 and (10000 <= Area and Area <= 18000):
-        #print(abs(cx - cx_ref))
-        #print(Area)
-        print("========= SS===========")
-        print("process :", ss_check_process, "result : ", ss_check_result)
-        print("Serial_No : ", Serial_No)
-        print("len : ", len(SS_SN.get()), len(QR_SN.get()))
-        if str(Serial_No) != '':
-            print("str 시리얼 테스트")
-        if abs(ss_cx - cx_ref) <= 1 and str(Serial_No) != '' and (qr_check_process == 1 and ss_check_process == 0) and ss_check_result == 2:
+        if str(Serial_No) != '' and (qr_check_process == 1 and ss_check_process == 0) and ss_check_result == 2 and PLC_signal == True:
+        #if abs(ss_cx - cx_ref) <= 1 and (35000 <= Area and Area <= 75000) and str(Serial_No) != '' and (qr_check_process == 1 and ss_check_process == 0) and ss_check_result == 2:
             # if abs(cx - cx_ref) <= 1 and (10000 <= Area and Area <= 18000):
             print("=====  판독 중  =====")
+            PLC_signal = False
             ss_check_process = 1
             if ss_check_make_folder == 0:
                 today = get_today()
@@ -709,6 +735,169 @@ def SSDetect(frame):
     return frame
 
 
+def start_detect():
+    global check_detect
+
+    check_detect = False
+
+
+def check_setting():
+    global check_set, store_location, pathname, setting
+
+    check_set = False
+    store_location_input = datapath.get()
+    setting.destroy()
+
+    if store_location_input != '':
+        parsing = store_location_input.split('/')
+        print(parsing)
+        for i in range(len(parsing)):
+            if parsing[i] != '':
+                pathname += parsing[i] + '/'
+                print(pathname)
+            elif parsing[i] == '/':
+                break
+            if i != 0:
+                make_folder(pathname)
+
+        store_location = pathname
+
+    PLC_signal_window()
+
+
+
+
+def PLC_sensor():
+    global PLC_signal, check_set
+
+    if check_set == False:
+        PLC_signal = True
+
+
+
+def PLC_signal_window():
+    ### 가상 PLC 신호창
+
+    PLC = Toplevel(root)
+    PLC.geometry("400x300")
+    PLC.title("Setting Window")
+    PLC.configure(bg="#ebebeb")
+
+    Label(PLC, text="PLC 신호 전송", font="돋움체", bg="#ebebeb", bd=2, width=25, height=2, relief="groove",
+          anchor=CENTER).place(x=0, y=0)
+    Button(PLC, text="PLC 신호 전송", font="돋움체", relief="raised", overrelief="solid", bg="#ebebeb", \
+           width=20, height=10, bd=3, padx=2, pady=2, command=PLC_sensor).place(x=10, y=50)
+
+
+def box_setting():
+    global qr_x1, qr_y1, qr_x2, qr_y2
+    global LU_X, LU_Y, RD_X, RD_Y
+    global box_set
+
+    #if box_set == False:
+    qr_x1 = eval(LU_X.get())
+    qr_y1 = eval(LU_Y.get())
+    qr_x2 = eval(RD_X.get())
+    qr_y2 = eval(RD_Y.get())
+
+    LU_X.delete(0, END)
+    LU_Y.delete(0, END)
+    RD_X.delete(0, END)
+    RD_Y.delete(0, END)
+
+    box_set = True
+
+
+def box_resetting():
+    global box_set, count_mouse
+    global qr_x1, qr_y1, qr_x2, qr_y2
+    global LU_X, LU_Y, RD_X, RD_Y
+
+    box_set = False
+    count_mouse = 0
+
+
+def setting_window():
+    global datapath, setting, box_label
+    global LU_X, LU_Y, RD_X, RD_Y
+    global box_set
+    ### 설정창
+
+    setting = Toplevel(root)
+    setting.geometry("800x600")
+    setting.title("Setting Window")
+    setting.configure(bg="#ebebeb")
+    qr_width, qr_height = 1920, 1080
+
+    Label(setting, text="QR 사각박스 설정", font="돋움체", bg="#ebebeb", bd=2, width=25, height=2, relief="groove",\
+          anchor=CENTER).place(x=110, y=0)
+
+    box_list = ["LU", "RD"]
+    position_list = ["X", "Y"]
+    for i in range(2):
+        Label(setting, text=box_list[i], height=2, width=15, fg="red", relief="groove", bg="#ebebeb",
+              font="Helvetica 13 bold").place(x=10 + (i * 220), y=65, relx=0.01, rely=0.01)
+        Label(setting, text=position_list[i], height=4, width=5, fg="red", relief="groove", bg="#ebebeb",
+              font="Helvetica 8 bold").place(x=5, y=(qr_height / 8) + (i * 61), relx=0.01, rely=0.01)
+        Label(setting, text=position_list[i], height=4, width=5, fg="red", relief="groove", bg="#ebebeb",
+              font="Helvetica 8 bold").place(x=215, y=(qr_height / 8) + (i * 61), relx=0.01, rely=0.01)
+
+    Button(setting, text="사각박스 설정하기", font="돋움체", relief="raised", overrelief="solid", bg="#ebebeb", \
+           width=47, height=2, bd=3, padx=2, pady=2, command=box_setting).place(x=10 , y=270)
+
+    ###사각박스 엔트리
+    ##LU
+    LU_X = Entry(setting, width=5, relief="groove", font="Helvetica 35 bold")
+    LU_X.place(x=45, y=(qr_height / 8) + 0 , relx=0.01, rely=0.01)
+
+    LU_Y = Entry(setting, width=5, relief="groove", font="Helvetica 35 bold")
+    LU_Y.place(x=45, y=(qr_height / 8) + 61 , relx=0.01, rely=0.01)
+
+    ##RU
+    RD_X = Entry(setting, width=5, relief="groove", font="Helvetica 35 bold")
+    RD_X.place(x=255, y=(qr_height / 8) + 0, relx=0.01, rely=0.01)
+
+    RD_Y = Entry(setting, width=5, relief="groove", font="Helvetica 35 bold")
+    RD_Y.place(x=255, y=(qr_height / 8) + 61, relx=0.01, rely=0.01)
+
+    #사각박스 확인
+    box_label = Label(setting)
+    box_label.place(x=530, y=100)
+
+
+    Label(setting, text="설정한 사각박스 확인", font="돋움체", bg="#ebebeb", bd=2, width=25, height=2, relief="groove", \
+          anchor=CENTER).place(x=500, y=0)
+    Button(setting, text="사각박스 재설정하기", font="돋움체", relief="raised", overrelief="solid", bg="#ebebeb", \
+           width=47, height=2, bd=3, padx=2, pady=2, command=box_resetting).place(x=410, y=270)
+
+    # 데이터 저장 경로 설정
+    Label(setting, text="데이터 저장 경로 설정", font="돋움체", bg="#ebebeb", bd=2, width=25, height=2, relief="groove", \
+          anchor=CENTER).place(x=0, y=440)
+    datapath = Entry(setting, width=35, relief="groove", font="Helvetica 35 bold")
+    datapath.place(x=0, y=490, relx=0.001, rely=0)
+
+    #설정 완료 버튼
+    Button(setting, text="설정 완료", font="Helvetica 13 bold", relief="groove", overrelief="solid", bg="#ebebeb", \
+           bd=3, padx=2, pady=2, command=check_setting).pack(side=BOTTOM, fill=X)
+
+
+def mouse_position(event):
+    global LU_X, LU_Y, RD_X, RD_Y, count_mouse
+    global box_set
+
+    print("===== 마우스 포지션 출력 =====")
+    print("click - ", event.x, event.y)
+
+    if box_set == False:
+        if count_mouse == 0:
+            LU_X.insert(20, event.x)
+            LU_Y.insert(20, event.y)
+            count_mouse += 1
+        elif count_mouse == 1:
+            RD_X.insert(20, event.x)
+            RD_Y.insert(20, event.y)
+            count_mouse += 1
+
 def execute():
     global qr_label, sticker_label, root
     global QR_SN, QR_P1, QR_P2, QR_P3, QR_P4, QR_P5
@@ -718,13 +907,12 @@ def execute():
     root = Tk()
 
     root.bind('<Escape>', lambda e: root.quit())
+    root.bind("<Double-Button-1>", mouse_position)
     qr_label = Label(root)
     qr_label.place(y=10, anchor=NW)
 
     sticker_label = Label(root)
     sticker_label.place(x=960, y=10)
-
-    width, height = 640, 480
 
     qr_width, qr_height = 1920, 1080
     root.title("Check_QR")
@@ -789,6 +977,7 @@ def execute():
     SS_P5 = Entry(root, width=19, relief="groove", font="Helvetica 50 bold")
     SS_P5.place(x=1178, y=(qr_height / 3) + 140 + (5 * 80), relx=0.01, rely=0.01)
 
+    setting_window()
     read_frame()
     root.mainloop()
 
