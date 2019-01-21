c3f5ef8552ea7ad62d51cf7131f655c0fc2fd1a5
Jay Lee <lyj911111@naver.com>
2019-01-21 10:38:04 +0700

    시리얼 통신테스트 프로토콜 ready , OK 테스트

--- _testcodes/Serial_통신_테스트_아두이노로/serialTest.py
+++ _testcodes/Serial_통신_테스트_아두이노로/serialTest.py
@@ -6,10 +6,17 @@ ser = serial.Serial(
     parity=serial.PARITY_NONE,\
     stopbits=serial.STOPBITS_ONE,\
     bytesize=serial.EIGHTBITS,\
-    timeout=1
 )
 
 while True:
     if ser.readable():
-        res = ser.readline()
-        print(res.decode()[:len(res)-1])    # 자동으로 포함되어있는 개행을 제거하기 위해 [:len(res)-1] 를 추가함.
+        res = ser.readline()                #
+
+        result = res.decode()[:len(res)-2]    # 자동으로 포함되어있는 개행을 제거하기 위해 [:len(res)-2] 를 추가함 : 즉, \r\n 을 제거함.
+
+        print(result)
+
+        if result == 'ready':
+            print('OK')
+        else:
+            print('NG')
