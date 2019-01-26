bd8919bc86076ab12eb1a63c847235be4862950b
Jay Lee <lyj911111@naver.com>
2019-01-26 10:22:47 +0700

    카메라 on

--- AceVision_GUI_EXE_conversion/RivetDetect/RivetDetect.py
+++ AceVision_GUI_EXE_conversion/RivetDetect/RivetDetect.py
@@ -319,11 +319,11 @@ def read_frame():
     global HOST, PORT, PLC_rx, PLC_tx_OK, PLC_tx_NG
     global ser, sock, protocol, port_num, set
 
-    '''
+
     webCamShow(cap1.read(), cam1_label, 1)
     webCamShow(cap2.read(), cam2_label, 2)
     webCamShow(cap3.read(), cam3_label, 3)
-    '''
+
 
     if check_set == False:
         if Serial_No != pre_Serial_No:
