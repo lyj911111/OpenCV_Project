af4487ae8b72b5cd065cfbc5128b7bf66bf833ba
Jay Lee <lyj911111@naver.com>
2019-02-01 15:42:08 +0700

    처음 비전카메라 사가 넘겨준 코드, 카메라 번호선택 인자를 받음,

---
+++ _testcodes/GIGE 카메라 (HIKVISION) 영상화면출력/업체측 원본코드.py
@@ -0,0 +1,166 @@
+# -- coding: utf-8 --
+
+import sys
+import time
+import msvcrt
+import numpy as np
+import cv2
+
+from ctypes import *
+
+sys.path.append("../MvImport")
+from MvCameraControl_class import *
+
+# ch: 开一个40MB的图像数据缓存大小 | en: Open a 40MB Image Buffer Size
+
+if __name__ == "__main__":
+
+    deviceList = MV_CC_DEVICE_INFO_LIST()
+    tlayerType = MV_GIGE_DEVICE | MV_USB_DEVICE
+    
+    # ch:枚举设备 | en:Enum device
+    ret = MvCamera.MV_CC_EnumDevices(tlayerType, deviceList)
+    if ret != 0:
+        print ("enum devices fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    if deviceList.nDeviceNum == 0:
+        print ("find no device!")
+        sys.exit()
+
+    print ("Find %d devices!" % deviceList.nDeviceNum)
+
+    for i in range(0, deviceList.nDeviceNum):
+        mvcc_dev_info = cast(deviceList.pDeviceInfo[i], POINTER(MV_CC_DEVICE_INFO)).contents
+        if mvcc_dev_info.nTLayerType == MV_GIGE_DEVICE:
+            print ("\ngige device: [%d]" % i)
+            strModeName = ""
+            for per in mvcc_dev_info.SpecialInfo.stGigEInfo.chModelName:
+                strModeName = strModeName + chr(per)
+            print ("device model name: %s" % strModeName)
+
+            nip1 = ((mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0xff000000) >> 24)
+            nip2 = ((mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x00ff0000) >> 16)
+            nip3 = ((mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x0000ff00) >> 8)
+            nip4 = (mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x000000ff)
+            print ("current ip: %d.%d.%d.%d\n" % (nip1, nip2, nip3, nip4))
+        elif mvcc_dev_info.nTLayerType == MV_USB_DEVICE:
+            print ("\nu3v device: [%d]" % i)
+            strModeName = ""
+            for per in mvcc_dev_info.SpecialInfo.stUsb3VInfo.chModelName:
+                if per == 0:
+                    break
+                strModeName = strModeName + chr(per)
+            print ("device model name: %s" % strModeName)
+
+            strSerialNumber = ""
+            for per in mvcc_dev_info.SpecialInfo.stUsb3VInfo.chSerialNumber:
+                if per == 0:
+                    break
+                strSerialNumber = strSerialNumber + chr(per)
+            print ("user serial number: %s" % strSerialNumber)
+
+    nConnectionNum = input("please input the index number of the device to connect:")
+
+    if int(nConnectionNum) >= deviceList.nDeviceNum:
+        print ("intput error!")
+        sys.exit()
+
+    # ch:创建相机实例 | en:Creat Camera Object
+    cam = MvCamera()
+    
+    # ch:选择设备并创建句柄 | en:Select device and create handle
+    stDeviceList = cast(deviceList.pDeviceInfo[int(nConnectionNum)], POINTER(MV_CC_DEVICE_INFO)).contents
+
+    ret = cam.MV_CC_CreateHandle(stDeviceList)
+    if ret != 0:
+        print ("create handle fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    # ch:打开设备 | en:Open device
+    ret = cam.MV_CC_OpenDevice(MV_ACCESS_Exclusive, 0)
+    if ret != 0:
+        print ("open device fail! ret[0x%x]" % ret)
+        sys.exit()
+    
+    # ch:探测网络最佳包大小(只对GigE相机有效) | en:Detection network optimal package size(It only works for the GigE camera)
+    if stDeviceList.nTLayerType == MV_GIGE_DEVICE:
+        nPacketSize = cam.MV_CC_GetOptimalPacketSize()
+        if int(nPacketSize) > 0:
+            ret = cam.MV_CC_SetIntValue("GevSCPSPacketSize",nPacketSize)
+            if ret != 0:
+                print ("Warning: Set Packet Size fail! ret[0x%x]" % ret)
+        else:
+            print ("Warning: Get Packet Size fail! ret[0x%x]" % nPacketSize)
+
+    # ch:设置触发模式为off | en:Set trigger mode as off
+    ret = cam.MV_CC_SetEnumValue("TriggerMode", MV_TRIGGER_MODE_OFF)
+    if ret != 0:
+        print ("set trigger mode fail! ret[0x%x]" % ret)
+        sys.exit()
+    
+    # ch:设置图像像素格式为mono8 | en：Set pixel format as mono8
+    ret = cam.MV_CC_SetEnumValue("PixelFormat", 0x01080001)
+    if ret != 0:
+        print("Set pixel format fail! ret[0x%x]"% ret)
+        sys.exit()
+    
+    # ch:开始取流 | en:Start grab image
+    ret = cam.MV_CC_StartGrabbing()
+    if ret != 0:
+        print ("start grabbing fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    global MAX_BUF_SIZE 
+    MAX_BUF_SIZE = 1024 * 1024 * 40
+    mv_frame_info = MV_FRAME_OUT_INFO_EX()
+    memset(byref(mv_frame_info), 0, sizeof(mv_frame_info))
+    data_buf = (c_ubyte * MAX_BUF_SIZE)()       
+    
+    # ch: 获取一帧图像 | en: Get one frame
+    ret = cam.MV_CC_GetOneFrameTimeout(data_buf, MAX_BUF_SIZE, mv_frame_info, 1000)
+    if ret != 0:
+        print("Get one frame fail! ret[0x%x]"% ret)
+        sys.exit()
+    
+    # ch: 保存RGB裸数据至opencv | en: Save RGB raw data to numpy array for opencv
+    data_buf_ch = data_buf[0:mv_frame_info.nFrameLen:1]
+	
+    data_buf_copy = np.array(data_buf_ch)
+    
+    #print(mv_frame_info.nFrameLen)
+    #data_r = data_buf_copy[0:mv_frame_info.nFrameLen:3]
+    #data_g = data_buf_copy[1:mv_frame_info.nFrameLen:3]
+    #data_b = data_buf_copy[2:mv_frame_info.nFrameLen:3]
+    data_mono_arr = data_buf_copy.reshape(mv_frame_info.nHeight, mv_frame_info.nWidth)
+    #data_r_arr = data_r.reshape(mv_frame_info.nHeight, mv_frame_info.nWidth)
+    #data_g_arr = data_g.reshape(mv_frame_info.nHeight, mv_frame_info.nWidth)
+    #data_b_arr = data_b.reshape(mv_frame_info.nHeight, mv_frame_info.nWidth)
+    
+    numArray = np.zeros([mv_frame_info.nHeight, mv_frame_info.nWidth, 3],"uint8")
+    
+    numArray[:, :, 2] = data_mono_arr
+    numArray[:, :, 1] = data_mono_arr
+    numArray[:, :, 0] = data_mono_arr
+    
+    cv2.imshow("",numArray)
+    cv2.waitKey()
+    # ch:停止取流 | en:Stop grab image
+    ret = cam.MV_CC_StopGrabbing()
+    if ret != 0:
+        print ("stop grabbing fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    # ch:关闭设备 | Close device
+    ret = cam.MV_CC_CloseDevice()
+    if ret != 0:
+        print ("close deivce fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    # ch:销毁句柄 | Destroy handle
+    ret = cam.MV_CC_DestroyHandle()
+    if ret != 0:
+        print ("destroy handle fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    del data_buf
