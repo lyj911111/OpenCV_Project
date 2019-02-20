dac630b9dd5cec866d4087adb5a328804892a706
Jay Lee <lyj911111@naver.com>
2019-02-20 16:37:02 +0700

    리벳 원형탐지로 찾기

--- _testcodes/원모양 탐지 (리벳의 원형만을 탐지 테스트코드)/White_Rivet_Detect_by_CircleDetector.py
+++ _testcodes/원모양 탐지 (리벳의 원형만을 탐지 테스트코드)/White_Rivet_Detect_by_CircleDetector.py
@@ -14,6 +14,7 @@ def houghCircle():
 
     imgray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY) # 그레이 이미지로 바꿔서 실행해야함.
 
+
     # param1 = 250, param2 = 20
                                  # 원본과 비율 / 찾은 원들간의 최소 중심거리 / param1, param2를 조절해 원을 찾음
     circles = cv2.HoughCircles(imgray, cv2.HOUGH_GRADIENT, 1, 10, param1=250, param2=20, minRadius=5, maxRadius=10)
@@ -23,16 +24,22 @@ def houghCircle():
 
         print(circles)
 
+        cnt = 0
         for i in circles[0, :]:
-            cv2.circle(img1, (i[0], i[1]), i[2], (0, 0, 255), 2)
+            cv2.circle(img1, (i[0], i[1]), i[2], (0, 0, 255), 2)    # 원 외곽 컨투어 표시.
+            cnt = cnt + 1
+
+        for i in circles[0, :]:
+            cv2.circle(img1, (i[0], i[1]), 3, (255, 0, 40), -1)     # 원의 중심점을 표시
 
         #cv2.imshow('ori',img2)
         cv2.imshow('HoughCircle', img1)
-
+        print(cnt)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     else:
         print('원을 찾을 수 없음')
+
 
 houghCircle()
