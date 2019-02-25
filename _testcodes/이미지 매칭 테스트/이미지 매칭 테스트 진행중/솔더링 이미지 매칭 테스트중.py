259333f065d6d7237d6d9f9e7755ca16b49aec93
Jay Lee <lyj911111@naver.com>
2019-02-25 17:24:03 +0700

    솔더링 이미지 매칭갯수 출력 완료

--- _testcodes/이미지 매칭 테스트/이미지 매칭 테스트 진행중/솔더링 이미지 매칭 테스트중.py
+++ _testcodes/이미지 매칭 테스트/이미지 매칭 테스트 진행중/솔더링 이미지 매칭 테스트중.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-List = []
+# before_point (이전좌표)
 bfr_pt = 0
 
 # 검출할 영역(관심영역)을 지정 (start point to end point).
@@ -10,7 +10,7 @@ s_pt = (500, 30)
 e_pt = (900, 900)
 
 # 확률이 떨어지는 이하 매칭률을 제외
-min = 6
+min = 0
 
 
 # 원본 불러오기.
@@ -38,15 +38,13 @@ loc = np.where(res1 >= threshold)
 
 cnt = 0
 cnt_list = []
-k = 0
 for pt in zip(*loc[::-1]):
 
     if cnt == 0:
         cnt_list = []
 
-    if pt < e_pt and (pt[0] + w1, pt[1] + h1) > s_pt:       # 관심영역(ROI)으로 판독 제한.
+    if (pt[0] > s_pt[0] and pt[1] > s_pt[1]) and ((pt[0] + w1 < e_pt[0]) and (pt[1] + h1 < e_pt[1])):       # 관심영역(ROI)으로 판독 제한.
 
-        k = k+8
         cnt = cnt + 1           # 매칭률에 따라 얼마나 매칭시켰는지 갯수를 셈.
         str_cnt = str(cnt)
 
@@ -55,32 +53,24 @@ for pt in zip(*loc[::-1]):
 
             cv2.rectangle(result, pt, (pt[0] + w1, pt[1] + h1), (0, 0, 255), 1)     # 판독위치 마킹.
             cnt_list.append(cnt)
-            max_cnt = str(max(cnt_list))
+            max_cnt = str(max(cnt_list))        # 판독 갯수 중 최대값 저장.
 
             print(cnt_list)
             print(max_cnt)
 
-            # 연습 출력용.
-            cv2.putText(result, 'loop:' + str_cnt, (pt[0]+k, pt[1]+k), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
-
-
             # 초기에 before_point가 없으므로 있을경우에 if문 진입.
             if bfr_pt:
 
-                print("="*20)
-                #print("x좌표의 이전좌표 - 현재좌표 차이: ", abs(bfr_pt[0] - pt[0]))
-                print("y좌표의 이전좌표 - 현재좌표 차이: ", abs(bfr_pt[1] - pt[1]))
-
-                # y좌표가 급격히 변하는 부분일떄.
-                if (abs(bfr_pt[1] - pt[1]) > 10) :
+                # y좌표가 급격히 변하는 부분일때.
+                if (abs(bfr_pt[1] - pt[1]) > 10) or (abs(bfr_pt[0] - pt[0]) > 10) :
                     str_cnt = str(cnt)
-                    cv2.putText(result, 'loop:' + max_cnt, bfr_pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
+                    cv2.putText(result, 'match:' + max_cnt, bfr_pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
                     print("ok!!!")
                     cnt = 0
                     k = 0
 
         bfr_pt = pt
-cv2.putText(result, 'loop:' + max_cnt, (pt), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
+cv2.putText(result, 'match:' + max_cnt, bfr_pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
 
 
 
@@ -100,7 +90,6 @@ for pt in zip(*loc[::-1]):
         if cnt > 5:
             cv2.rectangle(result, pt, (pt[0] + w2, pt[1] + h2), (0,255,0), 1)
 
-#print(List)
 
 cv2.imshow('res.png',result)
 cv2.waitKey(0)
