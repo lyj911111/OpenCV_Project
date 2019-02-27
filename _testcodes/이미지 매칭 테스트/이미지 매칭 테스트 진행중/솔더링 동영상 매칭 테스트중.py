6abf6c277ea1469b8ff2da995ac1ed9fee368fe8
Jay Lee <lyj911111@naver.com>
2019-02-27 16:48:17 +0700

    솔더링 이미지 매칭 동영상 fps를 좀더 빠르게

--- _testcodes/이미지 매칭 테스트/이미지 매칭 테스트 진행중/솔더링 동영상 매칭 테스트중.py
+++ _testcodes/이미지 매칭 테스트/이미지 매칭 테스트 진행중/솔더링 동영상 매칭 테스트중.py
@@ -1,132 +1,104 @@
-import cv2
 import numpy as np
-import time
-from matplotlib import pyplot as plt
+import cv2
 
-# 카메라 불러오기, 해상도 지정.
+# 해상도 결정, 출력.
 cap0 = cv2.VideoCapture(0)
-cap0.set(3, 4608)
-cap0.set(4, 3288)
-print("현재 해상도 %d x %d" %(cap0.get(3), cap0.get(4)))
+cap0.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) # Width 4608
+cap0.set(cv2.CAP_PROP_FRAME_HEIGHT, 960) # Height 3288
+print("첫번째 카메라 현재 해상도 %d x %d" %(cap0.get(3), cap0.get(4)))
+
 
 cap1 = cv2.VideoCapture(1)
-cap1.set(3, 4608)
-cap1.set(4, 3288)
-print("현재 해상도 %d x %d" %(cap0.get(3), cap0.get(4)))
+cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 4608) # Width 4608
+cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 3288) # Height 3288
+print("두번째 카메라 현재 해상도 %d x %d" %(cap1.get(3), cap1.get(4)))
 
 # 검출할 영역(관심영역)을 지정 (start point to end point of Rectangle Box).
-s_pt = (500, 30)
-e_pt = (900, 900)
+s_pt = (30, 200)
+e_pt = (1200, 900)
 
 # before_point (이전좌표)
 bfr_pt = 0
 
-# 메인실행 함수.
 def execute():
-    global bfr_pt
 
-    # 프레임 불러오기.
-    #img_rgb = cv2.imread('2.png')
-    while True:
 
+    while (True):
+        global bfr_pt
+
+        # 프레임 읽기
         ret, frame = cap1.read()
-        img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # gray로 변환.
-        result = frame.copy()                               # 결과출력을 위한 원본 복사
+        # 화면 크기 조절 (본인에게 맞는 해상도 조절)
+        result = cv2.resize(frame, (1280, 960), interpolation=cv2.INTER_LINEAR)
+        img_gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)                          # gray로 변환.
 
         # 관심영역(ROI, Range of Interest) 지정.
         result = cv2.rectangle(result, s_pt, e_pt, (255, 0, 0), 2)
-        cv2.putText(result, 'ROI', s_pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(result, 'ROI', (s_pt[0], s_pt[1]-3), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
         # 이미지 매칭할 template 불러옴. (2개의 template으로 이미지 매칭)
-        template1 = cv2.imread('template1.jpg',0)
-        template2 = cv2.imread('template2.jpg',0)
-
+        template1 = cv2.imread('Vtemplate1.jpg', 0)
+        template2 = cv2.imread('template2.jpg', 0)
 
         # 매칭할 template의 가로, 세로 사이즈를 return
         w1, h1 = template1.shape[::-1]
         w2, h2 = template2.shape[::-1]
 
-
         # 1번째 template 매칭.
         res1 = cv2.matchTemplate(img_gray, template1, cv2.TM_CCOEFF_NORMED)
         # 이미지 매칭률을 결정함.
-        threshold = 0.7
+        threshold = 0.79
         loc = np.where(res1 >= threshold)
 
-        print("loc?", loc)
+        # 이미 매칭을 시작.
+        cnt = 0
+        max_cnt = 0
+        cnt_list = []
+        for pt in zip(*loc[::-1]):
 
-        try:
-            cnt = 0
-            cnt_list = []
-            for pt in zip(*loc[::-1]):
+            if cnt == 0:
+                cnt_list = []
 
-                if cnt == 0:
-                    cnt_list = []
+            if (pt[0] > s_pt[0] and pt[1] > s_pt[1]) and (
+                    (pt[0] + w1 < e_pt[0]) and (pt[1] + h1 < e_pt[1])):  # 관심영역(ROI)으로 판독 제한.
 
-                if (pt[0] > s_pt[0] and pt[1] > s_pt[1]) and ((pt[0] + w1 < e_pt[0]) and (pt[1] + h1 < e_pt[1])):       # 관심영역(ROI)으로 판독 제한.
+                cnt = cnt + 1  # 매칭률에 따라 얼마나 매칭시켰는지 갯수를 셈.
 
-                    cnt = cnt + 1           # 매칭률에 따라 얼마나 매칭시켰는지 갯수를 셈.
+                cv2.rectangle(result, pt, (pt[0] + w1, pt[1] + h1), (255, 255, 0), 1)  # 판독위치 마킹.
+                cnt_list.append(cnt)
+                max_cnt = max(cnt_list)  # 판독 갯수 중 최대값 저장.
+                str_cnt = str(max_cnt)  # string 으로 변환
 
-                    cv2.rectangle(result, pt, (pt[0] + w1, pt[1] + h1), (255, 255, 0), 1)     # 판독위치 마킹.
-                    cnt_list.append(cnt)
-                    max_cnt = str(max(cnt_list))        # 판독 갯수 중 최대값 저장.
+                # 초기에 before_point가 없으므로, 있을경우에 if문 진입.
+                if bfr_pt:
 
-                    # 초기에 before_point가 없으므로 있을경우에 if문 진입.
-                    if bfr_pt:
+                    # x, y 좌표가 급격히 변하는 부분일때.
+                    if (abs(bfr_pt[1] - pt[1]) > 10) or (abs(bfr_pt[0] - pt[0]) > 10):
+                        cv2.putText(result, 'match:' + str_cnt, bfr_pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 1,
+                                    cv2.LINE_AA)
+                        cnt = 0  # 갯수를 다시 초기화
+                bfr_pt = pt  # 이전 좌표값 저장.
 
-                        # y좌표가 급격히 변하는 부분일때.
-                        if (abs(bfr_pt[1] - pt[1]) > 10) or (abs(bfr_pt[0] - pt[0]) > 10) :
-                            cv2.putText(result, 'match:' + max_cnt, bfr_pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 1, cv2.LINE_AA)
-                            cnt = 0
-                    bfr_pt = pt
-            cv2.putText(result, 'match:' + max_cnt, bfr_pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 1, cv2.LINE_AA)
-        except:
-            pass
+                print('max_cnt:', max_cnt)
 
-        #
-        # # 2번째 template 매칭.
-        # res2 = cv2.matchTemplate(img_gray,template2,cv2.TM_CCOEFF_NORMED)
-        # # 이미지 매칭률을 결정함.
-        # threshold = 0.75
-        # loc = np.where(res2 >= threshold)
-        #
-        # cnt = 0
-        # cnt_list = []
-        # for pt in zip(*loc[::-1]):
-        #
-        #     if cnt == 0:
-        #         cnt_list = []
-        #
-        #     if (pt[0] > s_pt[0] and pt[1] > s_pt[1]) and (
-        #             (pt[0] + w2 < e_pt[0]) and (pt[1] + h2 < e_pt[1])):  # 관심영역(ROI)으로 판독 제한.
-        #
-        #         cnt = cnt + 1  # 매칭률에 따라 얼마나 매칭시켰는지 갯수를 셈.
-        #
-        #         cv2.rectangle(result, pt, (pt[0] + w2, pt[1] + h2), (0, 255, 0), 1)  # 판독위치 마킹.
-        #         cnt_list.append(cnt)
-        #         max_cnt = str(max(cnt_list))  # 판독 갯수 중 최대값 저장.
-        #
-        #         # 초기에 before_point가 없으므로 있을경우에 if문 진입.
-        #         if bfr_pt:
-        #
-        #             # y좌표가 급격히 변하는 부분일때.
-        #             if (abs(bfr_pt[1] - pt[1]) > 10) or (abs(bfr_pt[0] - pt[0]) > 10):
-        #                 cv2.putText(result, 'match:' + max_cnt, bfr_pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
-        #                 cnt = 0
-        #         bfr_pt = pt
-        # cv2.putText(result, 'match:' + max_cnt, bfr_pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
-
-        # 화면 크기 조절
-        result = cv2.resize(result, (640 * 2, 480 * 2), interpolation=cv2.INTER_LINEAR)
+        # 맨 마지막 좌표 match 출력을 위한 조건문.
+        if max_cnt:
+            str_cnt = str(max_cnt)
+            cv2.putText(result, 'match:' + str_cnt, bfr_pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 1, cv2.LINE_AA)
+        else:
+            print('no matched with template 1')  # 아무것도 match 되지 않았을 때
 
         #cv2.namedWindow('window title', cv2.WINDOW_NORMAL)
-        #cv2.imshow('window title', img_gray)
-        cv2.imshow('res.png', result)
+        #cv2.imshow('window title', gray)
+        cv2.imshow('Test', result)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-if __name__ == "__main__":
+    # When everything done, release the capture
+    cap1.release()
+    cv2.destroyAllWindows()
 
-    #카메라 번호 선택 0~2
+if __name__ == "__main__":
+    # 메인함수
     execute()
