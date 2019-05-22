9a6a6589a1ff0e915eab5296375f28d6e98cd028
Jay Lee <lyj911111@naver.com>
2019-05-22 09:17:56 +0700

    노출값, gain값 트렉바 적용, 컨트롤 가능

--- _AceVision_testcodes/중국Csray_Mono_Gige카메라_연속프레임_취득/GxAcquireSoftTrigger.py
+++ _AceVision_testcodes/중국Csray_Mono_Gige카메라_연속프레임_취득/GxAcquireSoftTrigger.py
@@ -4,7 +4,7 @@
         pip 19.1.1
 
     기본 모듈:
-        pip install opencv-python==3.4.5
+        pip install opencv-python==3.4.5.20
         pip install Pillow==5.4.1
         pip install numpy==1.16.0
         pip install image==1.5.27
@@ -18,12 +18,17 @@
         2. 경로 변경 -> cd "CSR_V1.0.2_Python\api\" (api가 있는 곳 까지)
         3. 빌드      -> python setup.py build
         4. 패키지설치 -> python setup.py install
+        5. 재부팅 후 import gxipy 확인.
 
 '''
 # gxipy version : 1.0.1808.9101
 import gxipy as gx
 from PIL import Image
 import cv2
+
+
+def nothing(x):
+    pass
 
 
 # 컬러 카메라 취득
@@ -115,10 +120,10 @@ def main():
     # open the first device
     cam = device_manager.open_device_by_index(1)
 
-    # set exposure / 노출값(exposure Time) 조절
+    # set exposure / 노출값(exposure Time) default
     cam.ExposureTime.set(10000)
 
-    # set gain / 선명도 조절
+    # set gain / 선명도 조절 default
     cam.Gain.set(5.0)
 
     # send software trigger command
@@ -128,7 +133,19 @@ def main():
     # start data acquisition
     cam.stream_on()
 
+    # 트렉바 생성
+    cv2.namedWindow('trackbarwindow')
+    cv2.createTrackbar('exposure', 'trackbarwindow', 10000, 100000, nothing)
+    cv2.createTrackbar('gain', 'trackbarwindow', 5, 255, nothing)
+
     while True:
+
+        # 트렉바 적용, 노출값(exposure Time) 조절, 선명도 트렉바 컨트롤
+        exposure = cv2.getTrackbarPos('exposure', 'trackbarwindow')
+        gain = cv2.getTrackbarPos('gain', 'trackbarwindow')
+        cam.ExposureTime.set(exposure)
+        cam.Gain.set(gain)
+
         # camera is color / 컬러일 때
         if cam.PixelColorFilter.is_implemented() is True:
             acq_color(cam, 1)
@@ -138,10 +155,8 @@ def main():
             cv2.imshow('array', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):                   #  'q'를 누르면 종료.
-            # stop acquisition
-            cam.stream_off()
-            # close device
-            cam.close_device()
+            cam.stream_off()	# stop acquisition
+            cam.close_device()	# close device
             break
 
 if __name__ == "__main__":
