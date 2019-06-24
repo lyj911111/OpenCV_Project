db2ecc98ef7dc11c1ab0f25ee03dfbfd86bba091
Jay Lee <lyj911111@naver.com>
2019-06-24 14:58:54 +0700

    이미지 왜곡을 제거후 펼쳐버림

--- _testcodes/왜곡된 이미지를 펼치기 테스트/Perspective Transformation.py
+++ _testcodes/왜곡된 이미지를 펼치기 테스트/Perspective Transformation.py
@@ -7,7 +7,7 @@ img = cv2.imread('hough.jpg')
 rows, cols, ch = img.shape
 
 a = [[65,15],[481,13],[28,439],[508,445]]
-b = [[0, 0],[300, 0],[0,300],[300,300]]
+b = [[0, 0],[1000, 0],[0,500],[1000,500]]   # 왼쪽위점, 오른쪽위점, 왼쪽아래점, 오른쪽아래점
 
 pts1 = np.float32(a)
 pts2 = np.float32(b)
@@ -19,7 +19,7 @@ img = cv2.circle(img, (a[3][0], a[3][1]), 3, (0,0,255),-1)
 #cv2.circle(img, c, 5, (55, 255, 55), -1)
 
 M = cv2.getPerspectiveTransform(pts1, pts2)
-dst = cv2.warpPerspective(img, M, (300, 300))
+dst = cv2.warpPerspective(img, M, (1000, 500))  # 변환후 크기 (x좌표, y좌표)
 
 cv2.imshow('imgage',img)
 cv2.imshow('dst', dst)
